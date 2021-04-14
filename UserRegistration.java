1fcc47519e3a8401b685a1838762d59229204948
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-04-14 20:38:07 +0530

    Added: Welcome to User Registration program

---
+++ UserRegistration.java
@@ -0,0 +1,11 @@
+//Welcome to User Registration program
+//package com.bridgelabz;
+
+public class UserRegistration {
+
+	public static void main(String[] arg) {
+		System.out.println("Welcome to User Registration program");
+
+	}
+
+}
